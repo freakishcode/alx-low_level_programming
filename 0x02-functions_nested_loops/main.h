5bcf6e3a1d252d40e138d4acb56d32d54a184784
freakishcode <ajibolabakare@gmail.com>
2023-08-16 04:38:30 -0700

    created a main.h file

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,4 @@
-ifndef main_h
+#ifndef main_h
 #define main_h
 
 int _putchar(char);
@@ -15,4 +15,4 @@ int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
 
-#endif}
+#endif
